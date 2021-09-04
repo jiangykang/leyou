71d68051dc2fd77bea9aeed94209fe11eb172715
jiangykang <jiangyongkang1995@163.com>
2021-09-04 20:32:16 +0800

    改造上传图片到fdfs

--- ly-upload/src/main/java/com/leyou/service/UploadService.java
+++ ly-upload/src/main/java/com/leyou/service/UploadService.java
@@ -1,8 +1,12 @@
 package com.leyou.service;
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.leyou.controller.UploadController;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,6 +18,9 @@ import java.util.List;
 
 @Service
 public class UploadService {
+
+    @Autowired
+    FastFileStorageClient storageClient;
 
     private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
 
@@ -37,18 +44,26 @@ public class UploadService {
                 logger.info("上传失败，文件内容不符合要求");
                 return null;
             }
-            //2、保存图片
-            //2.1、生成保存目录
-            File dir = new File("D:\\heima\\upload");
-            if (!dir.exists()) {
-                dir.mkdirs();
-            }
-            //2.2、保存图片
-            file.transferTo(new File(dir, file.getOriginalFilename()));
+//            //2、保存图片
+//            //2.1、生成保存目录
+//            File dir = new File("D:\\heima\\upload");
+//            if (!dir.exists()) {
+//                dir.mkdirs();
+//            }
+//            //2.2、保存图片
+//            file.transferTo(new File(dir, file.getOriginalFilename()));
+//
+//            //2.3、拼接图片地址
+//            String url = "http://image.leyou.com/upload/" + file.getOriginalFilename();
+//            return url;
 
-            //2.3、拼接图片地址
-            String url = "http://image.leyou.com/upload/" + file.getOriginalFilename();
-            return url;
+            //2、将图片上传到FastDFS
+            //2.1、获取文件后缀名
+            String extension = StringUtils.substringAfterLast(file.getOriginalFilename(),".");
+            //2.2、上传
+            StorePath storePath = this.storageClient.uploadFile(file.getInputStream(), file.getSize(), extension, null);
+            //2.3、返回完整路径
+            return "http://image.leyou.com:8088/" + storePath.getFullPath();
         } catch (Exception e) {
             e.printStackTrace();
             return null;
